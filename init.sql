5216e868406ab157b2b3db58400f0a037d489246
Ajith.kumar <ajithkumar@pheonixsolutions.com>
2025-09-02 09:47:29 +0530

    modified changes into folder

--- init.sql
+++
@@ -1,8 +0,0 @@
--- Runs automatically on first MySQL start
-CREATE TABLE IF NOT EXISTS greetings (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  message VARCHAR(255) NOT NULL
-);
-
-INSERT INTO greetings (message)
-VALUES (' Database connected successfully  Welcome India');
